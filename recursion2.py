38e3c81314506263e5a1f951afc159114889810c
Ashwin Chandran <ashwinchandran13@gmail.com>
2020-04-21 20:30:24 +0530

    Recursion Function 2

---
+++ recursion2.py
@@ -0,0 +1,18 @@
+def explode(word):
+    if len(word) <= 1:
+        return word
+    else:
+        return word[0] + ' ' + explode(word[1:]) 
+
+def removeDups(word):   
+    if len(word) <= 1:    
+        return word
+    elif word[0] == word[1]:       
+        return removeDups(word[1:])   
+    else:
+        return word[0] + removeDups(word[1:])   
+
+#print(explode('hello'))
+word = 'aasshhwwiinnn'
+print(word)
+print(removeDups(word))
